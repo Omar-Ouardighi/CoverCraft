5d74aef622b8748b9424895120792cf5567332f1
Omar <ouardighiomar@gmail.com>
2023-11-27 15:41:06 +0100

    test

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import DirectoryLoader, PyPDFDirectoryLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 import docx2txt
 from PyPDF2 import PdfReader
@@ -17,12 +17,11 @@ class Chatbot:
         self.underlying_embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         self.llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
 
-        qa_template = """
-        
+        qa_template = """       
 You are an invaluable assistant entrusted with crafting personalized cover letters. Armed with the user's resume and the specifics of the job description,
- your mission is to meticulously tailor the cover letter to highlight precisely how the candidate's qualifications align with the job requirements. 
- Additionally, if provided, incorporate any extra insights about the company to infuse the cover letter with specific details, such as projects, values, or achievements.
-  The goal is to create a compelling narrative that not only demonstrates a perfect fit for the job but also showcases a deep understanding of the unique aspects of the hiring company.
+your mission is to meticulously tailor the cover letter to highlight precisely how the candidate's qualifications align with the job requirements. 
+Additionally, if provided, incorporate any extra insights about the company to infuse the cover letter with specific details, such as projects, values, or achievements.
+The goal is to create a compelling narrative that not only demonstrates a perfect fit for the job but also showcases a deep understanding of the unique aspects of the hiring company.
 
         context: {context}
         =========
@@ -55,7 +54,7 @@ You are an invaluable assistant entrusted with crafting personalized cover lette
         return vectorstore
     
     def build_chain(_self, vectorstore, top_k):
-        chain = ConversationalRetrievalChain.from_llm(llm = _self.llm, 
+        chain = RetrievalQA.from_chain_type(llm = _self.llm, 
                                             retriever = vectorstore.as_retriever(search_kwargs={"k": top_k}),
                                             combine_docs_chain_kwargs={'prompt': _self.QA_PROMPT},
                                             return_source_documents=True)
